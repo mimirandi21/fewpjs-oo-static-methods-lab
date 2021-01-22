b019096d2c6052503f709a805f07d9fcfd34b264
Randi Schreiner <mimirandi21@gmail.com>
2021-01-22 11:50:42 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 class Formatter {
-  //add static methods here
+	static capitalize(s) {
+		return s.charAt(0).toUpperCase() + s.slice(1);
+	}
+
+	static sanitize(s) {
+		return s.replace(/[!@#$%^&*{}()]/g, "");
+	}
+
+	static titleize(string) {
+		let splitted = string.split(" ");
+		let excluded = [
+			"the",
+			"a",
+			"an",
+			"but",
+			"of",
+			"and",
+			"for",
+			"at",
+			"by",
+			"from",
+		];
+		let newArray = [];
+		newArray.push(Formatter.capitalize(splitted[0]));
+
+		for (let i = 1; i < splitted.length; i++) {
+			if (excluded.includes(splitted[i])) {
+				newArray.push(splitted[i]);
+			} else {
+				newArray.push(Formatter.capitalize(splitted[i]));
+			}
+		}
+		return newArray.join(" ");
+	}
 }
